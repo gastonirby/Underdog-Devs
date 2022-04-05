7533858b13b7fe4460394637755c0bde6b7d2516
Anthony Coman <acoman@yahoo.com>
2022-04-05 15:41:46 -0500

    added a modal, added spacing, changed UX. Used dummy data.

--- src/components/pages/Reviews/Reviews.js
+++ src/components/pages/Reviews/Reviews.js
@@ -1,44 +1,83 @@
-import React, { useState } from 'react';
-import { Input, Button, Menu, Dropdown } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Input, Button, Menu, Dropdown, Modal } from 'antd';
 import axiosWithAuth from '../../../utils/axiosWithAuth';
 
 function Reviews() {
   const { TextArea } = Input;
   const [toggle, setToggle] = useState(false);
-  const dummyData = [];
+  const [btnTitle, setBtnTitle] = useState('');
+  const [displayModal, setDisplayModal] = useState(false);
+  const [data, setData] = useState();
+  const dummyData = [
+    {
+      mentor_id: '12',
+      mentor_name: 'asdjk ajskdla',
+    },
+    {
+      mentor_id: '123',
+      mentor_name: 'Anthony Coman',
+    },
+    {
+      mentor_id: '234',
+      mentor_name: 'Ted Kim',
+    },
+    {
+      mentor_id: '345',
+      mentor_name: 'Phil Gailbraith',
+    },
+  ];
 
-  const onSubmit = e => {
+  useEffect(() => {
     axiosWithAuth()
-      .post('/profile', { content: 'review goes here' })
+      .get('/mentorsss') //will get mentors to select
       .then(res => {
-        //success message
-        console.log('Sent successfully');
-        console.log(res);
+        setData(res.data);
       })
       .catch(err => {
         console.log(err);
       });
-    alert('Sent successfully message');
+  }, []);
+
+  const onSubmit = e => {
+    if (e.target.key) {
+      return null;
+    }
+    // axiosWithAuth()
+    //   .post('/profile', { content: 'string', mentor_id: 'string' })
+    //   .then(res => {
+    //     //success message
+    //     console.log('Sent successfully');
+    //     console.log(res);
+    //   })
+    //   .catch(err => {
+    //     console.log(err);
+    //   });
   };
 
   const click = () => {
     setToggle(!toggle);
   };
-  const draft = () => {
-    alert('Draft saved');
+  const handleBtnTitleChange = e => {
+    setBtnTitle(e.key);
   };
 
   const menu = (
     <Menu>
-      <Menu.Item>
-        <p>1st menu item</p>
+      <Menu.Item
+        style={{ padding: '10%' }}
+        key={'Choose a Mentor'}
+        onClick={handleBtnTitleChange}
+      >
+        {' '}
       </Menu.Item>
-      <Menu.Item>
-        <p>2st menu item</p>
-      </Menu.Item>
-      <Menu.Item>
-        <p>3st menu item</p>
-      </Menu.Item>
+      {dummyData.map(obj => {
+        // data.map
+        return (
+          <Menu.Item key={obj.mentor_name} onClick={handleBtnTitleChange}>
+            {obj.mentor_name}
+          </Menu.Item>
+        );
+      })}
     </Menu>
   );
 
@@ -49,16 +88,40 @@ function Reviews() {
       {toggle && (
         <div>
           <Dropdown overlay={menu} trigger={['click']}>
-            <Button>{Menu.Item ? `${Menu.Item.p}` : 'Mentor'}</Button>
+            <Button style={{ marginBottom: '2%' }}>
+              {btnTitle.length ? btnTitle : 'Choose a Mentor'}
+            </Button>
           </Dropdown>
-          <TextArea rows={4} />
-          <Button onClick={draft}>Stored as draft</Button>
-          <Button onClick={onSubmit}>Submit</Button>
+          <TextArea rows={6} style={{ marginBottom: '2%' }} />
+          <Button
+            type="primary"
+            onClick={() => setDisplayModal(!displayModal)}
+            style={{ marginBottom: '2%' }}
+          >
+            Submit
+          </Button>
           <div>
             <Button onClick={click}>Cancel</Button>
           </div>
         </div>
       )}
+      <Modal
+        title="Submission"
+        visible={displayModal}
+        footer={[
+          <Button key="back" onClick={() => setDisplayModal(!displayModal)}>
+            Back
+          </Button>,
+          <Button key="submit" type="primary" onClick={onSubmit}>
+            Submit
+          </Button>,
+        ]}
+      >
+        <p>
+          Are you sure you want to submit? You cannot edit or delete your review
+          after submitting.
+        </p>
+      </Modal>
     </>
   );
 }
